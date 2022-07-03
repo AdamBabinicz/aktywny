2edfeb7ddd328cd4da448780f9144c88196a272d
babinicz <babinicz@onet.pl>
2022-07-03 19:41:32 +0200

    zmiany

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/header/Header";
 import AboutUs from "./components/aboutUs/AboutUs";
@@ -10,48 +11,54 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import CookieConsent from "react-cookie-consent";
+import ScrollToTop from "./ScrollToTop";
+import Scroll from "./components/Scroll";
 
 function App() {
   return (
-    <div className="holder">
-      <Header />
-      <AboutUs />
-      <Skills />
-      <Stats />
-      <Work />
-      <WorkProcess />
-      <Services />
-      <Testimonials />
-      <Contact />
-      <Footer />
-      <CookieConsent
-        debug={true}
-        location="bottom"
-        style={{
-          background: "#333",
-          textAlign: "left",
-          paddingBottom: "1rem",
-          fontSize: "16px",
-          fontFamily: "Gideon Roman",
-        }}
-        buttonStyle={{
-          color: "#333",
-          background: "#fff",
-          fontSize: "18px",
-          fontFamily: "Gideon Roman",
-          marginRight: "1rem",
-        }}
-        buttonText="OK, rozumiem"
-        expires={365}
-      >
-        "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
-        Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
-        indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
-        dotyczących cookies oznacza, że będą one zamieszczane w Państwa
-        urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
-        ustawień dotyczących cookies."
-      </CookieConsent>
-    </div>
+    <Router>
+      <div className="holder">
+        <ScrollToTop />
+        <Scroll />
+        <Header />
+        <AboutUs />
+        <Skills />
+        <Stats />
+        <Work />
+        <WorkProcess />
+        <Services />
+        <Testimonials />
+        <Contact />
+        <Footer />
+        <CookieConsent
+          debug={true}
+          location="bottom"
+          style={{
+            background: "#333",
+            textAlign: "left",
+            paddingBottom: "1rem",
+            fontSize: "16px",
+            fontFamily: "Gideon Roman",
+          }}
+          buttonStyle={{
+            color: "#333",
+            background: "#fff",
+            fontSize: "18px",
+            fontFamily: "Gideon Roman",
+            marginRight: "1rem",
+          }}
+          buttonText="OK, rozumiem"
+          expires={365}
+        >
+          "W ramach naszej witryny stosujemy pliki cookies w celu świadczenia
+          Państwu usług na najwyższym poziomie, w tym w sposób dostosowany do
+          indywidualnych potrzeb. Korzystanie z witryny bez zmiany ustawień
+          dotyczących cookies oznacza, że będą one zamieszczane w Państwa
+          urządzeniu końcowym. Możecie Państwo dokonać w każdym czasie zmiany
+          ustawień dotyczących cookies."
+        </CookieConsent>
+      </div>
+    </Router>
   );
 }
 
