c6fe869612f8e2f5699880366e3183592fb10c46
babinicz <babinicz@onet.pl>
2023-05-03 14:15:05 +0200

    &nbsp;

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -21,7 +21,7 @@ const Header = () => {
         <div className="header__content text__center text__light flex flex__center">
           <div className="header__content--right">
             <h1 className="header__title fw__6">
-              Moja aktywność umysłowa i fizyczna
+              Moja aktywność umysłowa i&nbsp;fizyczna
             </h1>
             <p className="para__text">
               Mila Radości - Dumne Warchoły 2022 - Bieg Wolontariatu Radomskego
@@ -33,7 +33,7 @@ const Header = () => {
           <Modal show={modal} close={Toggle} title="Mila Radości">
             <p>
               Wszyscy uczestnicy biegu na jedną milę otrzymali koszulki
-              biało-czerwone i pamiątkowy medal.
+              biało-czerwone i&nbsp;pamiątkowy medal.
             </p>
             <br />
             <div className="medal">
